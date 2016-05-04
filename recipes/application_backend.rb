ee419eaa98d3484ad71446aeefe117b77771d0d1
Richard Shade <rshade@rightscale.com>
2016-05-04 09:17:14 -0500

    changing php to tomcat

--- recipes/application_backend.rb
+++ recipes/application_backend.rb
@@ -7,31 +7,30 @@ class Chef::Recipe
 end
 
 # Validate application name
-RsApplicationPhp::Helper.validate_application_name(node['rs-application_php']['application_name'])
 
 # Check if there is at least one load balancer in the deployment serving the application name
-if find_load_balancer_servers(node, node['rs-application_php']['application_name']).empty?
-  raise "No load balancer servers found in the deployment serving #{node['rs-application_php']['application_name']}!"
+if find_load_balancer_servers(node, node['rsc_tomcat']['application_name']).empty?
+  raise "No load balancer servers found in the deployment serving #{node['rsc_tomcat']['application_name']}!"
 end
 
 # Put this backend into consideration during tag queries
 log 'Tagging the application server to put it into consideration during tag queries...'
-machine_tag "application:active_#{node['rs-application_php']['application_name']}=true" do
+machine_tag "application:active_#{node['rsc_tomcat']['application_name']}=true" do
   action :create
 end
 
 
 # Send remote recipe request
-log "Running recipe '#{node['rs-application_php']['remote_attach_recipe']}' on all load balancers" +
- " with tags 'load_balancer:active_#{node['rs-application_php']['application_name']}=true'..."
+log "Running recipe '#{node['rsc_tomcat']['remote_attach_recipe']}' on all load balancers" +
+ " with tags 'load_balancer:active_#{node['rsc_tomcat']['application_name']}=true'..."
 
 remote_recipe "HAProxy Frontend - chef" do
-  tags "load_balancer:active_#{node['rs-application_php']['application_name']}=true"
+  tags "load_balancer:active_#{node['rsc_tomcat']['application_name']}=true"
   attributes( {'APPLICATION_BIND_IP' => "text:#{node['cloud']["private_ips"][0]}",
-      'APPLICATION_BIND_PORT' => "text:#{node['rs-application_php']['listen_port']}",
+      'APPLICATION_BIND_PORT' => "text:#{node['rsc_tomcat']['listen_port']}",
       'APPLICATION_SERVER_ID' => "text:#{node['rightscale']['instance_uuid']}",
-      'POOL_NAME' => "text:#{node['rs-application_php']['application_name']}",
-      'VHOST_PATH' => "text:#{node['rs-application_php']['vhost_path']}",
+      'POOL_NAME' => "text:#{node['rsc_tomcat']['application_name']}",
+      'VHOST_PATH' => "text:#{node['rsc_tomcat']['vhost_path']}",
       'APPLICATION_ACTION' => "text:attach"})
   action :run
 end
--- recipes/application_backend_detached.rb
+++ recipes/application_backend_detached.rb
@@ -7,23 +7,23 @@ class Chef::Recipe
 end
 
 # Validate application name
-RsApplicationPhp::Helper.validate_application_name(node['rs-application_php']['application_name'])
+RsApplicationTomcat::Helper.validate_application_name(node['rsc_tomcat']['application_name'])
 
 # Put this backend out of consideration during tag queries
 log 'Tagging the application server to take it out of consideration during tag queries...'
-machine_tag "application:active_#{node['rs-application_php']['application_name']}=false" do
+machine_tag "application:active_#{node['rsc_tomcat']['application_name']}=false" do
   action :create
 end
 
 # Send remote recipe request
-log "Running recipe '#{node['rs-application_php']['remote_detach_recipe']}' on all load balancers" +
-" with tags 'load_balancer:active_#{node['rs-application_php']['application_name']}=true'..."
+log "Running recipe '#{node['rsc_tomcat']['remote_detach_recipe']}' on all load balancers" +
+" with tags 'load_balancer:active_#{node['rsc_tomcat']['application_name']}=true'..."
 
 remote_recipe "HAProxy Frontend - chef" do
-  tags "load_balancer:active_#{node['rs-application_php']['application_name']}=true"
+  tags "load_balancer:active_#{node['rsc_tomcat']['application_name']}=true"
   attributes( {
       'APPLICATION_SERVER_ID' => "text:#{node['rightscale']['instance_uuid']}",
-      'POOL_NAME' => "text:#{node['rs-application_php']['application_name']}",
+      'POOL_NAME' => "text:#{node['rsc_tomcat']['application_name']}",
       'APPLICATION_ACTION' => "text:detach"})
   action :run
 end
