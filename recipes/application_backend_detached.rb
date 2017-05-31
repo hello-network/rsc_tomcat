dadc90f0c7756fdfbb0b15d73ab0b96b4d0428f2
Richard Shade <rshade@rightscale.com>
2017-05-31 10:28:22 -0500

    adding in pool list (#1)

--- recipes/application_backend_detached.rb
+++ recipes/application_backend_detached.rb
@@ -7,23 +7,24 @@ class Chef::Recipe
 end
 
 # Validate application name
-RsApplicationTomcat::Helper.validate_application_name(node['rsc_tomcat']['application_name'])
-
+RsApplicationTomcat::Helper.validate_application_name(app_name)
+node['rsc_tomcat']['application_pool_list'].split(',').each do |app_name|
 # Put this backend out of consideration during tag queries
 log 'Tagging the application server to take it out of consideration during tag queries...'
-machine_tag "application:active_#{node['rsc_tomcat']['application_name']}=false" do
+machine_tag "application:active_#{app_name}=false" do
   action :create
 end
 
 # Send remote recipe request
 log "Running recipe '#{node['rsc_tomcat']['remote_detach_recipe']}' on all load balancers" +
-" with tags 'load_balancer:active_#{node['rsc_tomcat']['application_name']}=true'..."
+" with tags 'load_balancer:active_#{app_name}=true'..."
 
 remote_recipe "HAProxy Frontend - chef" do
-  tags "load_balancer:active_#{node['rsc_tomcat']['application_name']}=true"
+  tags "load_balancer:active_#{app_name}=true"
   attributes( {
       'APPLICATION_SERVER_ID' => "text:#{node['rightscale']['instance_uuid']}",
-      'POOL_NAME' => "text:#{node['rsc_tomcat']['application_name']}",
+      'POOL_NAME' => "text:#{app_name}",
       'APPLICATION_ACTION' => "text:detach"})
   action :run
+end
 end
